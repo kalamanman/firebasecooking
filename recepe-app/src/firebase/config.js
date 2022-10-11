6374526a0426d575c1351b7fe1353cb7405026e5
unknown <newmankal@live.com>
2022-10-10 23:02:41 -0400

    firebase recipes instead of json-server

---
+++ recepe-app/src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDv357a-HNPcJg0H8SMcUG4p05tssYNO8M",
+    authDomain: "cooking-ninja-f8812.firebaseapp.com",
+    projectId: "cooking-ninja-f8812",
+    storageBucket: "cooking-ninja-f8812.appspot.com",
+    messagingSenderId: "137314678000",
+    appId: "1:137314678000:web:1639230ff3b9d7b36bfe35"
+  };
+
+
+
+
+
+
+
+
+firebase.initializeApp(firebaseConfig)
+
+export const storeHandle=firebase.firestore()
--- recepe-app/src/pages/home/Home.jsx
+++ recepe-app/src/pages/home/Home.jsx
@@ -1,12 +1,35 @@
 //styles
 import  './Home.css'
-import {useFetch} from '../../hooks/useFetch'
+import { storeHandle } from '../../firebase/config'
+import {useState,useEffect} from 'react'
 import List from '../../components/List'
 import  ThemeSelector  from '../../components/ThemeSelector'
 
 const Home = () => {
-    const url='http://localhost:3000/recipes'
-    const {data:recipes,error,isPending} = useFetch(url)
+    
+    const [recipes,setRecipes] = useState(null)
+    const [error,setError] = useState(null)
+    const [isPending,setIsPending] = useState(false)
+
+
+    useEffect(()=>{
+        setIsPending(true)
+        storeHandle.collection('recipes').get()
+        .then((snapshot)=>{
+          if(snapshot.empty){
+            setIsPending(false)
+            setError('No recipes were found')
+          }else{
+              let result =[]
+              snapshot.docs.forEach(doc=>{
+                result.push({id:doc.id, ...doc.data()})
+              })
+              setIsPending(false)
+              setRecipes(result)
+          }
+
+        })
+    },[])
 
   return (
 
