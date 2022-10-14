339507180c186c1a8239f96e9c514d495355122d
kalamanman <34838608+kalamanman@users.noreply.github.com>
2022-10-14 13:05:23 -0700

    Update config.js

--- recepe-app/src/firebase/config.js
+++ recepe-app/src/firebase/config.js
@@ -4,19 +4,7 @@ import 'firebase/firestore'
 const firebaseConfig = {//your config goes here
 
 
-    apiKey: "AIzaSyDv357a-HNPcJg0H8SMcUG4p05tssYNO8M",
-
-  authDomain: "cooking-ninja-f8812.firebaseapp.com",
-
-  projectId: "cooking-ninja-f8812",
-
-  storageBucket: "cooking-ninja-f8812.appspot.com",
-
-  messagingSenderId: "137314678000",
-
-  appId: "1:137314678000:web:1639230ff3b9d7b36bfe35"
-
-  
+    
 
 
 
