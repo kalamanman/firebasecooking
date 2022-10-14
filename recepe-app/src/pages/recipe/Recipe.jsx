89604a40a0625d02c905f582a3413808f1980f2d
unknown <newmankal@live.com>
2022-10-13 17:46:33 -0700

    Added firbase

--- recepe-app/src/pages/recipe/Recipe.jsx
+++ recepe-app/src/pages/recipe/Recipe.jsx
@@ -3,22 +3,32 @@ import { useParams } from 'react-router-dom'
 //styles
 import './Recipe.css'
 import { useTheme } from '../../hooks/useTheme'
+import { storeHandle } from '../../firebase/config'
+import {useState} from 'react'
 
 
 const Recipe = () => {
 
   const {id} = useParams()
 
-  const url = `http://localhost:3000/recipes/${id}`
-  
-     const{data:recipe} =  useFetch(url)
+
+     const[recipe,setRecipe] =  useState(null)
+     const[error,setError] =  useState('')
   
    const{color,mode}= useTheme()
-
+   const findRecipe=(id)=>{
+    storeHandle.collection('recipes').doc(id).get()
+    .then(doc=>{
+      if(!doc.exists){ setError('Could not find that recipe')}
+    setRecipe(doc.data())
+    })
+   }
+   findRecipe(id)
+  
 
   return (
     <div className='container  min-vh-100 '  >
-   
+   {error && <div className='text-danger'  >{error}</div> }
     {recipe && 
        < div className="card my-2 text-light style='width: 10em;'" 
        style={{backgroundColor:mode ==='dark'?'grey':color}}
